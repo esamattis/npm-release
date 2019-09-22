0a0d5e326f6a3c95373510348c1b6f8c1885c367
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-09-22 18:29:34 +0300

    Sanity checks

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,7 @@ import * as core from "@actions/core";
 import { exec } from "@actions/exec";
 import { execSync } from "child_process";
 
-async function run() {
-    const tag = core.getInput("tag");
-    const type = core.getInput("type");
-
-    if (!["stable", "prerelease"].includes(type)) {
-        core.setFailed("unknown release type " + type);
-        return;
-    }
-
-    const npmToken = core.getInput("token");
-
-    if (!npmToken) {
-        core.setFailed("NPM_TOKEN input not set");
-        return;
-    }
-
-    await fs.writeFile(
-        PathUtils.join(process.env.HOME || "~", ".npmrc"),
-        `//registry.npmjs.org/:_authToken=${npmToken}`,
-    );
-
-    await exec("npm whoami");
-
+async function setPrereleaseVersion() {
     const packageFile = "./package.json";
     const pkg = JSON.parse((await fs.readFile(packageFile)).toString());
 
@@ -47,10 +25,47 @@ async function run() {
 
     await fs.writeFile(packageFile, JSON.stringify(pkg, null, "    "));
     console.log("Prerelease version: " + pkg.version);
+}
+
+async function run() {
+    const tag = core.getInput("tag") || "next";
+
+    if (/[a-z]+/.test(tag)) {
+        core.setFailed(`Invalid tag format "${tag}". Only a-z characters.`);
+        return;
+    }
+
+    const type: "stable" | "prerelease" = core.getInput("type") as any;
+
+    if (!["stable", "prerelease"].includes(type)) {
+        core.setFailed(
+            "You must set the 'type' input to 'stable' or 'prerelease'",
+        );
+        return;
+    }
+
+    const npmToken = core.getInput("token");
+
+    if (!npmToken) {
+        core.setFailed("'token' input not set");
+        return;
+    }
+
+    await fs.writeFile(
+        PathUtils.join(process.env.HOME || "~", ".npmrc"),
+        `//registry.npmjs.org/:_authToken=${npmToken}`,
+    );
+
+    await exec("npm whoami");
 
     await exec("npm ci");
-    await exec("npm test");
-    core.setOutput("time", new Date().toTimeString());
+
+    if (type === "prerelease") {
+        await setPrereleaseVersion();
+        await exec(`npm publish --tag ${tag}`);
+    } else {
+        await exec("npm publish");
+    }
 }
 
 run().catch(error => {
