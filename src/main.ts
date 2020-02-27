e8c4a4ee5de553d519c3205ad73c70cdd387cff6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-02-27 10:03:29 +0200

    Fix package name var

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,8 @@ async function setPrereleaseVersion() {
 async function exportReleaseVersion() {
     const packageFile = "./package.json";
     const pkg = JSON.parse((await fs.readFile(packageFile)).toString());
+    core.exportVariable("NPM_PACKAGE_NAME", pkg.name);
     core.exportVariable("NPM_RELEASE_VERSION", pkg.version);
-    core.exportVariable("NPM_PACKAGE_NAME", pkg.version);
 }
 
 async function isDir(path: string): Promise<boolean> {
